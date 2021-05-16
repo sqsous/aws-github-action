f6c8541ef58a78918bb7321766f34658e9e953f2
sqsous <84263110+sqsous@users.noreply.github.com>
2021-05-16 20:59:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,27 @@
-
-terraform {
-    required_version = ">= 0.12.26"
+provider "aws" {
+    region = "us-east-1"
+    access_key = "access-key"
+    secret_key = "secret-key"
 }
 
-output "hello_world" {
-    value = "Hello, World!"
+resource "aws_s3_bucket" "s3-sima" {
+    bucket = "s3-sima"
+    acl    = "private"
+
 }
 
+resource "aws_s3_bucket_object" "object1" {
+    bucket = "s3-sima"
+    key    = "test1.txt"
+    source = "test1.txt"
 
+    depends_on = [aws_s3_bucket.s3-sima]
+}
+
+resource "aws_s3_bucket_object" "object2" {
+    bucket = "s3-sima"
+    key    = "test2.txt"
+    source = "test2.txt"
+
+    depends_on = [aws_s3_bucket.s3-sima]
+}
